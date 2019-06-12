036185ce7a563c95a08db88d6582b2990e0bb123
parag <paraggarg66@yahoo.in>
2019-06-12 18:48:13 -0400

    Initial Commit

---
+++ day5lambtoncollegeswift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  day5lambtoncollegeswift
+//
+//  Created by Gurparkash Kaur on 2019-06-12.
+//  Copyright © 2019 Gurparkash Kaur. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
